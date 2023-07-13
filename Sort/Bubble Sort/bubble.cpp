f1894270352e046baf16eb01a9be14153a9327eb
Shuklashashi0706 <shuklashashi169@gmail.com>
2023-07-13 21:03:25 +0530

    Bubble Sort

--- Sort/Bubble Sort/bubble.cpp
+++ Sort/Bubble Sort/bubble.cpp
@@ -1,6 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    
+void bubble(int arr[],int n);
+int main()
+{
+  int n, i, j;
+  cout << "Enter array size:" << endl;
+  cin >> n;
+  int arr[n]; // Declare an array of size n
+  cout << "Enter elements:" << endl;
+  for (i = 0; i < n; i++)
+  {
+    cin >> arr[i]; // Input array elements
+  }
+  bubble(arr,n);
+   for(i=0;i<n;i++){
+    cout<<arr[i]<<"\t";
+  }
+  cout<<endl;
   return 0;
+}
+void bubble(int arr[],int n){
+  int temp,i,j;
+  for(i=0;i<n-1;i++){
+    for(j=0;j<n-i;j++){
+      if(arr[j]>arr[j+1]){
+        temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+      }
+    }
+  }
+ 
 }
