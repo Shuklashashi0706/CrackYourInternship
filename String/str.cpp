5cfe4425f2efd191805b9279efc2ac86aaa04feb
Shuklashashi0706 <shuklashashi169@gmail.com>
2023-12-20 06:21:52 +0530

    Palindrome

---
+++ String/str.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string pal;
+    getline(cin,pal);
+    int start=0,end=pal.size()-1;
+    int flag=1;
+    while(start <= (pal.size()/2)){
+        if(pal[start] != pal[end]){
+            flag=0;
+            break;
+        }
+        start++;
+        end--;
+    }
+    if(flag == 0){
+        cout<<"not a pallindrome"<<endl;
+    }
+    else{
+        cout<<"Pallindrome"<<endl;
+    }
+    return 0;
+}
