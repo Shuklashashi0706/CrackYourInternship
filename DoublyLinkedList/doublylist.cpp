33e1f220874382a63a02bab39bfdc0ebbfaed542
Shuklashashi0706 <shuklashashi169@gmail.com>
2024-06-08 17:48:51 +0530

    [Doubly linked list]

--- DoublyLinkedList/doublylist.cpp
+++ DoublyLinkedList/doublylist.cpp
@@ -59,6 +59,124 @@ node *createLinkedList(int arr[], int size, node *head)
     }
     return head;
 }
+node *insertAtpos(node *head, int data, int pos)
+{
+    if (pos == 0)
+    {
+        if (head == NULL)
+        {
+            head = new node(data);
+        }
+        else
+        {
+            node *temp = new node(data);
+            temp->next = head;
+            head->prev = temp;
+            head = temp;
+        }
+    }
+    else
+    {
+        node *curr = head;
+        while (--pos)
+        {
+            curr = curr->next;
+        }
+
+        if (curr->next == NULL)
+        {
+            node *temp = new node(data);
+            temp->prev = curr;
+            curr->next = temp;
+        }
+        else
+        {
+            node *temp = new node(data);
+            temp->next = curr->next;
+            temp->prev = curr;
+            curr->next = temp;
+            temp->next->prev = temp;
+        }
+    }
+    return head;
+}
+node *deletionAtStart(node *head)
+{
+    if (head != NULL)
+    {
+        node *temp = head;
+        head = head->next;
+        delete temp;
+        if (head)
+        {
+            head->prev = NULL;
+        }
+    }
+    return head;
+}
+node *deletionAtEnd(node *head)
+{
+    if (head != NULL)
+    {
+        if (head->next == NULL)
+        {
+            delete head;
+            head = NULL;
+        }
+        else
+        {
+            node *curr = head;
+            while (curr->next)
+            {
+                curr = curr->next;
+            }
+            curr->prev->next = NULL;
+            delete curr;
+        }
+    }
+    return head;
+}
+
+node *deletionAtpos(node *head, int pos)
+{
+    if (head != NULL)
+    {
+        // deletion at start
+        if (pos == 1)
+        {
+            if (head != NULL)
+            {
+                node *temp = head;
+                head = head->next;
+                delete temp;
+                if (head)
+                {
+                    head->prev = NULL;
+                }
+            }
+        }
+        else
+        {
+            node *curr = head;
+            while (--pos)
+            {
+                curr = curr->next;
+            }
+            if (curr->next == NULL) // means last node
+            {
+                curr->prev->next = NULL;
+                delete curr;
+            }
+            else
+            {
+                curr->prev->next = curr->next;
+                curr->next->prev = curr->prev;
+                delete curr;
+            }
+        }
+    }
+    return head;
+}
 
 int main()
 {
@@ -66,6 +184,13 @@ int main()
     int len = sizeof(arr) / sizeof(arr[0]);
     node *head = NULL;
     head = createLinkedList(arr, len, head);
+    int data = 5;
+    int pos = 4;
+    head = insertAtpos(head, data, pos);
+    head = deletionAtStart(head);
+    head = deletionAtEnd(head);
+    pos = 1;
+    head = deletionAtpos(head, pos);
     node *temp = head;
     while (temp)
     {
