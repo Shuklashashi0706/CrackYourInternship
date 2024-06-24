58229dae2c2880e1e501ce511d2b4e8c40bfe6c5
Shuklashashi0706 <shuklashashi169@gmail.com>
2024-06-24 19:20:22 +0530

    stack

--- Stack/stackSTL.cpp
+++ Stack/stackSTL.cpp
@@ -6,16 +6,6 @@ using namespace std;
 
 int main()
 {
-    vector<int> arr = {1, 2, 3, 4};
-    int n = arr.size();
-    for (int i = 0; i < n; i++)
-    {
-        arr.push_back(arr[i]);
-    }
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cout << arr[i] << "\t";
-    }
-    cout << endl;
+    cout << '4 ' - '0' << endl;
     return 0;
 }
